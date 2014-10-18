06c06bf8e57feb158acc3990e320e64ba9ff61ee
ohbyungkwon <obk6438@naver.com>
2014-10-18 15:38:07 +0900

    Apply single instance application!
    it will start with only one instance on your machine.

--- BeautifulTalk/App.xaml.cs
+++ BeautifulTalk/App.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using BeautifulTalk.Tray;
+using BeautifulTalkInfrastructure.Logger;
 using Hardcodet.Wpf.TaskbarNotification;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 
 namespace BeautifulTalk
@@ -25,6 +27,21 @@ namespace BeautifulTalk
         {
             base.OnStartup(e);
 
+            string strMutexName = string.Format("{0}.{1}.{2}", Environment.MachineName, Environment.UserName, "BeautifulTalk");
+
+            bool bCreateNew;
+            var mutex = new Mutex(true, strMutexName, out bCreateNew);
+
+            if (false == bCreateNew)
+            {
+                Application.Current.Shutdown();
+            }
+
+            this.InitializeInfra();
+        }
+
+        private void InitializeInfra()
+        {
             TalkBootstrapper bootStrapper = new TalkBootstrapper();
             bootStrapper.Run();
 
